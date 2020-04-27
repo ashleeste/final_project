ace43bb1f83ce4b67cf58c768c5ca0b0c7d71f9c
Ashlee Stephenson <ashleeste@gmail.com>
2020-04-26 21:47:08 -0600

    final project week 7

--- server/index.js
+++ server/index.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const app = express(); //initialize
+const express = require("express");
+const app = express();
 
-//how to serve up website: middleware (middle piece between input and output)
-app.use(express.static('public')); //read from file system and pull in static directory called public
+const port = process.env.PORT || 4000;
 
-app.use('/css', express.static(__dirname + '/public/css')); //adding css = http://localhost:3000/css
-app.use('/js', express.static(__dirname + '/public/src')); //adding js = http://localhost:3000/js
-app.use('/js', express.static(__dirname + '/public/images'))
-// also do with images if you add images - make sure they are in the public folder
+app.use(express.static("public"));
 
-//output that we want
-app.listen(3000, function() {
-    console.log('Server started at http://localhost:%s', 3000) 
+app.use("/css", express.static(__dirname + "/public/css")); // http://localhost:3000/css
+app.use("/js", express.static(__dirname + "/public/src")); // http://localhost:3000/js
+
+app.listen(port, function () {
+  console.log("Server started at http://localhost:%s", port);
 });
